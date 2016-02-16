464ca71da360306b5f6dfcb5e4a16db5f182ea84
Craig Morton <CraigMorton92@gmail.com>
2016-02-16 16:21:05 +0000

    added functions for 10 pre-written tests

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,72 @@
+def return_10()
+  return 10
+end
+
+def add(num1,num2)
+  return num1+num2
+end
+
+def subtract(num1,num2)
+  return num1-num2
+end
+
+def multiply(num1,num2)
+  return num1*num2
+end
+
+def divide(num1,num2)
+  return num1 / num2 
+end
+
+def length_of_string(string)
+  return string.length
+end
+
+def join_string(string1,string2)
+  return string1+string2
+end
+
+def add_string_as_number(string1, string2)
+  return string1.to_i + string2.to_i
+end
+
+def number_to_full_month_name(number)
+  case number
+  when 1
+    month = "January"
+  when 2
+    month = "February"
+  when 3
+    month = "March"
+  when 4
+    month = "April"
+  when 5
+    month = "May"
+  when 6
+    month = "June"
+  when 7
+    month = "July"
+  when 8
+    month = "August"
+  when 9
+    month = "September"
+  when 10
+    month = "October"
+  when 11
+    month = "November"
+  when 12
+    month = "December"
+  end
+  return month
+end
+
+def number_to_short_month_name(month_number)
+  month = number_to_full_month_name(month_number)
+  substring = month[0,3]
+  return substring
+end
+
+
+
+
+
